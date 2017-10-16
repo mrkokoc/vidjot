0f0109ead2c45e73704dfd5f93e66dc34cf3ed26
mrkokoc <ilia.guskov@gmail.com>
2017-10-16 19:05:47 +0300

    Add, Edit, Delete

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const methodOverride = require('method-override');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
@@ -9,9 +10,7 @@ const app = express();
 mongoose.Promise = global.Promise;
 
 // Mongoose connect
-mongoose.connect('mongodb://localhost/vidjot-dev', {
-    useMongoClient: true
-})
+mongoose.connect('mongodb://localhost/vidjot-dev', {useMongoClient: true})
         .then(() => console.log('MongoDB Connected...'))
         .catch(err => console.log(err));
 
@@ -29,6 +28,9 @@ app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+// Method Override Middleware
+app.use(methodOverride('_method'));
+
 // Index Route
 app.get('/', (req, res) => {
     const title = 'Welcome';
@@ -42,12 +44,34 @@ app.get('/about', (req, res) => {
     res.render('about');
 });
 
-// Ideas Route
+// Idea Index Page
 app.get('/ideas', (req, res) => {
-    res.render('ideas/ideas');
+    Idea.find({})
+            .sort({date: 'desc'})
+            .then(ideas => {
+                res.render('ideas/index', {
+                    ideas: ideas
+                });
+            });
 });
 
-// Process Form
+// Add Idea Form
+app.get('/ideas/add', (req, res) => {
+    res.render('ideas/add')
+});
+
+// Edit Idea Route
+app.get('/ideas/edit/:id', (req, res) => {
+    Idea.findOne({
+        _id: req.params.id
+    }).then(idea => {
+        res.render('ideas/edit', {
+            idea: idea
+        });
+    });
+});
+
+// Form Process
 app.post('/ideas', (req, res) => {
     let errors = [];
 
@@ -68,15 +92,39 @@ app.post('/ideas', (req, res) => {
             details: req.body.details
         });
     } else {
-        res.send('passed')
+        const newUser = {
+            title: req.body.title,
+            details: req.body.details,
+        };
+        new Idea(newUser).save().then(idea => {
+            res.redirect('/ideas')
+        })
     }
 });
 
-// Add Idea Form
-app.get('/ideas/add', (req, res) => {
-    res.render('ideas/add')
+// Edit Form Process
+app.put('/ideas/:id', (req, res) => {
+    Idea.findOne({
+        _id: req.params.id
+    }).then(idea => {
+        // new values
+        idea.title = req.body.title;
+        idea.details = req.body.details;
+
+        idea.save().then(idea => {
+            res.redirect('/ideas')
+        });
+    });
 });
 
+// Delete Idea
+app.delete('/ideas/:id', (req, res) => {
+    Idea.remove({
+        _id: req.params.id
+    }).then(() => {
+        res.redirect('/ideas')
+    });
+});
 
 const port = process.env.PORT || 5000;
 
