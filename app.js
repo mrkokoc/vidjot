5f8644f4d7c9d21dc47a17b4b98dde28537cfd3a
mrkokoc <ilia.guskov@gmail.com>
2017-10-18 02:19:08 +0300

    Passport Login & Register Local Strategy

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const methodOverride = require('method-override');
 const flash = require('connect-flash');
 const session = require('express-session');
 const mongoose = require('mongoose');
+const passport = require('passport');
 const bodyParser = require('body-parser');
 
 const app = express();
@@ -12,6 +13,9 @@ const app = express();
 // Load Routes
 const ideas = require('./routes/ideas');
 const users = require('./routes/users');
+
+// Passport Config
+require('./config/passport')(passport);
 
 // Map global promise
 mongoose.Promise = global.Promise;
@@ -44,6 +48,10 @@ app.use(session({
   saveUninitialized: true,
 }));
 
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Flash middleware
 app.use(flash());
 
@@ -53,6 +61,7 @@ app.use((req, res, next) => {
     res.locals.warning_msg = req.flash('warning_msg');
     res.locals.error_msg = req.flash('error_msg');
     res.locals.error = req.flash('error');
+    res.locals.user = req.user || null;
     next();
 });
 
