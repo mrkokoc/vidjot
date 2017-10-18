eadc9bb322e02d24ea2347fbf21ace377f2e4b22
mrkokoc <ilia.guskov@gmail.com>
2017-10-18 03:08:56 +0300

    Full Auth Control

--- routes/ideas.js
+++ routes/ideas.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const {ensureAuthenticated} = require('../helpers/auth');
 
 // Load Idea Model
 require('../models/Idea');
 const Idea = mongoose.model('ideas');
 
 // Idea Index Page
-router.get('/', (req, res) => {
-    Idea.find({})
+router.get('/', ensureAuthenticated, (req, res) => {
+    Idea.find({user: req.user.id})
             .sort({date: 'desc'})
             .then(ideas => {
                 res.render('ideas/index', {
@@ -18,23 +19,28 @@ router.get('/', (req, res) => {
 });
 
 // Add Idea Form
-router.get('/add', (req, res) => {
+router.get('/add', ensureAuthenticated, (req, res) => {
     res.render('ideas/add')
 });
 
 // Edit Idea Route
-router.get('/edit/:id', (req, res) => {
+router.get('/edit/:id', ensureAuthenticated, (req, res) => {
     Idea.findOne({
         _id: req.params.id
     }).then(idea => {
-        res.render('ideas/edit', {
-            idea: idea
-        });
+        if (idea.user !== req.user.id) {
+            req.flash('error_msg', 'Not Authorized');
+            res.redirect('/ideas');
+        } else {
+            res.render('ideas/edit', {
+                idea: idea
+            });
+        }
     });
 });
 
 // Form Process
-router.post('/', (req, res) => {
+router.post('/', ensureAuthenticated, (req, res) => {
     let errors = [];
 
     if (!req.body.title) {
@@ -57,6 +63,7 @@ router.post('/', (req, res) => {
         const newUser = {
             title: req.body.title,
             details: req.body.details,
+            user: req.user.id
         };
         new Idea(newUser).save().then(idea => {
             req.flash('success_msg', 'Idea added');
@@ -66,7 +73,7 @@ router.post('/', (req, res) => {
 });
 
 // Edit Form Process
-router.put('/:id', (req, res) => {
+router.put('/:id', ensureAuthenticated, (req, res) => {
     Idea.findOne({
         _id: req.params.id
     }).then(idea => {
@@ -82,7 +89,7 @@ router.put('/:id', (req, res) => {
 });
 
 // Delete Idea
-router.delete('/:id', (req, res) => {
+router.delete('/:id', ensureAuthenticated, (req, res) => {
     Idea.remove({
         _id: req.params.id
     }).then(() => {
